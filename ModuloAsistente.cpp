e0ae3b9679538391a3bd95cc6c46ee0b92e118b9
tobipp <tobippapdlta@gmail.com>
2020-12-05 17:44:13 -0300

    Vincular matricula veterianario con turnos en modulo asistente

--- ModuloAsistente.cpp
+++ ModuloAsistente.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <conio.h>
+#include "LibreriaModulos.h"
 
 /*
 Universidad Tecnologica Nacional
@@ -36,10 +37,10 @@ struct fecha
 
 struct turnos
 {
-    int matricula;
     fecha fec;
     int DNIduenio;
     char atencion[380];
+    int matriculaVet; //la matricula del veterinario que lo atiende
 };
 
 struct datosUsuario
@@ -52,12 +53,12 @@ struct datosUsuario
 
 struct datosVete
 {
-    char nomyApe[80];
+    char nomyApe[60];
     int matricula;
     int DNI;
     char telefono[25];
     int modulo; //1- Administrador. 2- Veterinario. 3- Asistente
-    char contraseña[10];
+    char contrasenia[10];
 };
 
 struct mascota
@@ -74,12 +75,12 @@ struct mascota
 //P R O T O T I P O S
 void AgregarMascota(FILE *archi);
 void ListarAnimales(FILE *archi);
-void RegistrarTurno(FILE *archi);
-void ListarTurno(FILE *archi);
+void RegistrarTurno(FILE *archi, FILE *archi2);
+void ListarTurno(FILE *archi, FILE *archi2);
 
 main()
 {
-    FILE *archivo, *archivo2;
+    FILE *archivo, *archivo2, *archivo3;
     int opcion, idx = 0, idxTurnos = 0, caso = 0;
 
     archivo = fopen("Mascotas.dat", "r+b");
@@ -137,12 +138,11 @@ main()
         printf("M o d u l o  d e  A s i s t e n c i a\n");
         printf("===========================================\n\n");
 
-        printf("1.- I n i c i a r   S e s i o n\n");
-        printf("2.- R e g i s t r a r   M a s c o t a\n");
-        printf("3.- R e g i s t r a r   T u r n o\n");
-        printf("4.- L i s t a d o  d e   A t e n c i o n e s   p o r   V e t e r i n a r i o   y   F e c h a\n\n");
+        printf("1.- R e g i s t r a r   M a s c o t a\n");
+        printf("2.- R e g i s t r a r   T u r n o\n");
+        printf("3.- L i s t a d o  d e   A t e n c i o n e s   p o r   V e t e r i n a r i o   y   F e c h a\n\n");
 
-        printf("5.- C e r r a r   l a   a p l i c a c i o n\n\n");
+        printf("4.- C e r r a r   l a   a p l i c a c i o n\n\n");
         printf("===========================================\n\n");
         printf("> ");
         scanf("%d", &caso);
@@ -151,14 +151,11 @@ main()
 
         switch (caso)
         {
+
         case 1:
 
-            break;
-
-        case 2:
-
             AgregarMascota(archivo);
-            printf("\n¿Desea ver el listado? (1- SI / 0- NO)");
+            printf("\nDesea ver el listado? (1- SI / 0- NO)");
             scanf("%d", &opcion);
 
             if (opcion == 1)
@@ -172,20 +169,21 @@ main()
 
             break;
 
-        case 3:
+        case 2:
+            system("cls");
 
             printf("Registrar Turnos");
-            RegistrarTurno(archivo2);
+            RegistrarTurno(archivo2, archivo3);
             break;
 
-        case 4:
+        case 3:
 
-            ListarTurno(archivo2);
+            ListarTurno(archivo2, archivo3);
 
             break;
         }
 
-    } while (caso != 5);
+    } while (caso != 4);
 }
 
 void AgregarMascota(FILE *archi)
@@ -246,7 +244,7 @@ void ListarAnimales(FILE *archi)
 
     fread(&vec, sizeof(vec), 1, archi);
 
-    while(!feof(archi))
+    while (!feof(archi))
     {
         system("cls");
         printf("\tL I S T A R  M A S C O T A S");
@@ -268,13 +266,28 @@ void ListarAnimales(FILE *archi)
     system("cls");
 }
 
-void RegistrarTurno(FILE *archi)
+void RegistrarTurno(FILE *archi, FILE *archi2)
 {
+    archi2 = fopen("Veterinarios.dat", "r+b");
     archi = fopen("Turnos.dat", "r+b");
     turnos reg;
+    datosVete regi;
+    bool matCorrecta;
 
-    printf("\n\nMatricula del Veterinario: ");
-    scanf("%d", &reg.matricula);
+    do
+    {
+        matCorrecta = true;
+        printf("\n\nMatricula del Veterinario: ");
+        scanf("%d", &reg.matriculaVet);
+
+        if (VerificarMatricula(reg.matriculaVet))
+        {
+            printf("Error: la matricula no corresponde a un veterinario registrado");
+            matCorrecta = false;
+            getch();
+        }
+
+    } while (!matCorrecta);
 
     printf("\nFecha de Turno: \n");
     printf("Dia: ");
@@ -299,33 +312,75 @@ void RegistrarTurno(FILE *archi)
     system("cls");
 }
 
-void ListarTurno(FILE *archi)
+void ListarTurno(FILE *archi, FILE *archi2)
 {
+    archi2 = fopen("Veterinarios.dat", "r+b");
     archi = fopen("Turnos.dat", "r+b");
     turnos reg;
+    datosVete regi;
+
+    int mat = 0;                //matricula
+    bool matEncontrada = false; //bandera
+    bool matValida = false;
 
     fread(&reg, sizeof(turnos), 1, archi);
 
-    system("cls");
-    printf("\tL I S T A D O S   D E   T U R N O S");
-    printf("\n=============================================\n");
-
-    while (!feof(archi))
+    do
     {
-        if (!feof(archi))
-        {
-            printf("\nFecha de Turno \n");
-            printf("=========================\n");
-            printf("Dia: %d", reg.fec.dia);
-            printf("\nMes: %d", reg.fec.mes);
-            printf("\nAnio: %d", reg.fec.anio);
-            printf("\nDNI: %d", reg.DNIduenio);
-            printf("\nDetalles: %s", reg.atencion);
-            printf("\n=========================\n");
+        matValida = true;
+        system("cls");
+        printf("\tL I S T A D O   D E   T U R N O S");
+        printf("\n=============================================\n");
 
-            fread(&reg, sizeof(turnos), 1, archi);
+        printf("Ingrese matricula de Veterinario: ");
+        scanf("%d", &mat);
+
+        if (VerificarMatricula(mat))
+        {
+            printf("Error: la matricula no corresponde a un veterinario registrado");
+            getch();
+            matValida = false;
+        }
+    } while (!matValida);
+
+    system("cls");
+
+    fread(&regi, sizeof(datosVete), 1, archi2);
+
+    while (!feof(archi2))
+    {
+        if (mat == regi.matricula)
+        {
+            matEncontrada = true;
+        }
+
+        fread(&regi, sizeof(datosVete), 1, archi2);
+    }
+
+    if (matEncontrada)
+    {
+        while (!feof(archi))
+        {
+            if (!feof(archi) and reg.matriculaVet == mat)
+            {
+                printf("\nFecha de Turno \n");
+                printf("=========================\n");
+                printf("Dia: %d", reg.fec.dia);
+                printf("\nMes: %d", reg.fec.mes);
+                printf("\nAnio: %d", reg.fec.anio);
+                printf("\nDNI: %d", reg.DNIduenio);
+                printf("\nDetalles: %s", reg.atencion);
+                printf("\n=========================\n");
+
+                fread(&reg, sizeof(turnos), 1, archi);
+            }
         }
     }
+    else
+    {
+        printf("No se encontraron turnos");
+    }
+
     getch();
 
     fclose(archi);
